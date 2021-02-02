cc4c902e7f1b5a02ffb9c0a98ae0b1dad1b66cfd
qiming <qiming@gatech.edu>
2021-02-02 19:36:48 +0800

    fix bugs

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pyodbc
 import os
 from config import log
+from utils import download_latest
 
 if __name__ == '__main__':
     log.info('Trying to connect...')
     try:
         conn = pyodbc.connect(os.environ['CONNECTION_STRING'])  # using username + password
+        download_latest(conn, log)
         conn.close()
     except Exception as e:
         log.error(e)
