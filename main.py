4eb8075d19775bc55a5654c34d454ec4d916230d
qiming <qiming@gatech.edu>
2021-02-02 19:29:00 +0800

    update odbc

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
-import pymssql
+import pyodbc
 import os
 from config import log
 
 if __name__ == '__main__':
-    # server    数据库服务器名称或IP
-    # user      用户名
-    # password  密码
-    # database  数据库名称
-    server = os.environ['SERVER']
-    database = os.environ['DATABASE']
-    user = os.environ['USER']
-    password = os.environ['PASSWORD']
-    log.info(f'Trying to connect {server}')
+    log.info('Trying to connect...')
     try:
-        conn = pymssql.connect(server, user, password, database)  # using username + password
+        conn = pyodbc.connect(os.environ['CONNECTION_STRING'])  # using username + password
         conn.close()
     except Exception as e:
         log.error(e)
