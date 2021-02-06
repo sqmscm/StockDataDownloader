dd12c1c68974598073aae87a78810103050b39d3
qiming <qiming@gatech.edu>
2021-02-06 19:14:53 +0800

    Support for the auto-pause database

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import pyodbc
-import os
+from time import sleep
 from config import log, conn_str
 from utils import download_latest
 
 if __name__ == '__main__':
     log.info('Trying to connect...')
-    try:
-        conn = pyodbc.connect(conn_str)  # using username + password
-        download_latest(conn, log)
-        conn.close()
-    except Exception as e:
-        log.error(e)
-        log.info('Connection Failed')
+    conn_trails = 0
+    conn = None
+    while conn_trails < 5:
+        try:
+            conn = pyodbc.connect(conn_str)
+            break
+        except Exception as e:
+            log.error(e)
+            conn_trails += 1
+            log.info(f'Connection Failed {conn_trails}.')
+            sleep(30)
+    else:
+        log.error('Could not connect to database.')
+        exit()
+
+    download_latest(conn, log)
+    conn.close()
+
